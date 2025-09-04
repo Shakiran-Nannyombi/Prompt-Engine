c5f4fad1c733c3868e5fd252e1a6ae0367082e19
Shakiran-Nannyombi <devkiran256@gmail.com>
2025-09-04 18:02:06 +0300

    Added api intergration for both coauch and refiner

--- backend/app/fastApi/routes.py
+++ backend/app/fastApi/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api import prompt_coach
+from app.fastApi import prompt_coach, prompt_refiner
 
 api_router = APIRouter()
 
 api_router.include_router(prompt_coach.router, prefix="/coaching", tags=["coaching"])
+api_router.include_router(prompt_refiner.router, prefix="/refiner", tags=["refiner"])
