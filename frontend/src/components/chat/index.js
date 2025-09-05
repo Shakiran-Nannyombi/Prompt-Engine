2f704c796a5f78f65dbaab0b833370cb5fa73697
Shakiran-Nannyombi <devkiran256@gmail.com>
2025-09-05 17:58:11 +0300

    Added chat componenets

---
+++ frontend/src/components/chat/index.js
@@ -0,0 +1,9 @@
+// Chat Components Export
+export { default as ChatInterface } from './ChatInterface.vue'
+export { default as ChatMessageList } from './ChatMessageList.vue'
+export { default as ChatMessage } from './ChatMessage.vue'
+export { default as ChatInput } from './ChatInput.vue'
+
+// Legacy components (existing)
+export { default as CoachChatInterface } from './CoachChatInterface.vue'
+export { default as RefinerChatInterface } from './RefinerChatInterface.vue'
